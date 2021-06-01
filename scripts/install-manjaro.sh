cf6b8280351ef5152807e3b9af72f74957879639
Luke Short <ekultails@gmail.com>
2021-05-31 22:16:21 -0600

    Enable swap
    This will later get picked up by 'genfstab' when setting up the
    partition mounts.

--- scripts/install-manjaro.sh
+++ scripts/install-manjaro.sh
@@ -45,6 +45,7 @@ echo "Mounting partitions..."
 mount -t btrfs -o subvol=/,noatime,nodiratime ${DEVICE}4 /mnt
 mkdir -p /mnt/boot/efi
 mount -t vfat ${DEVICE}2 /mnt/boot/efi
+swapon ${DEVICE}3
 mkdir -p /mnt/tmp /mnt/var/log /mnt/var/tmp
 
 for i in tmp var/log var/tmp; do
