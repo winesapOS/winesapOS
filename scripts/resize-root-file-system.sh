cf0994788c96056fe0e4954c272dc0ad9a9efe79
Luke Short <ekultails@gmail.com>
2021-09-12 13:00:40 -0600

    Resize LUKS root file system
    Detect if LUKS is being used and then expand that physical
    partition size, the LUKS virtual partition, and the Btrfs file
    system.

--- scripts/resize-root-file-system.sh
+++ scripts/resize-root-file-system.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 root_partition=$(mount | grep 'on \/ ' | awk '{print $1}')
-root_partition_number=$(echo ${root_partition} | grep -o -P "[0-9]+")
-root_device=$(echo ${root_partition} | sed s'/[0-9]//'g)
-growpart ${root_device} ${root_partition_number}
+
+if [[ "${root_partition}" == "/dev/mapper/cryptroot" ]]; then
+    root_partition_shortname=$(lsblk -o name,label | grep mlgs-luks | awk '{print $1}' | grep -o -P '[a-z]+.*')
+    root_partition_number=$(echo ${root_partition_shortname} | grep -o -P "[0-9]+")
+    root_device=$(echo "/dev/${root_partition_shortname}" | sed s'/[0-9]//'g)
+    growpart ${root_device} ${root_partition_number}
+    cryptsetup resize ${root_partition}
+else
+    root_partition_number=$(echo ${root_partition} | grep -o -P "[0-9]+")
+    root_device=$(echo ${root_partition} | sed s'/[0-9]//'g)
+    growpart ${root_device} ${root_partition_number}
+fi
+
 btrfs filesystem resize max /
