865832e6c9af8bd3034c34468ddaea9dbfe045af
Thijzer <thijs.dp@gmail.com>
2022-03-31 19:43:44 +0200

    [img building] moved to zip and sh512 checksum

--- scripts/winesapos-build.sh
+++ scripts/winesapos-build.sh
@@ -3,10 +3,10 @@ export \
     WINESAPOS_CREATE_DEVICE=true \
     WINESAPOS_BUILD_IN_VM_ONLY=false
 
-bash scripts/winesapos-install.sh
+zsh scripts/winesapos-install.sh
 
 ## we need to introduce versioning here
 ARCHIVE=winesapos.img.gz
-gzip --keep --fast winesapos.img
-mv winesapos.img.gz /workdir/output/
-sha256sum /workdir/output/winesapos.img.gz > /workdir/output/winesapos-sha256sum.txt
+zip -s 1900m winesapos.img.zip winesapos.img
+mv winesapos.img.zip /workdir/output/
+sha512sum /workdir/output/winesapos-performance* > /workdir/output/winesapos-sha512sum.txt
