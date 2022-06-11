434c1166a9ab50ac1c7a145f5c17a1f6c4c8c3ea
Luke Short <ekultails@gmail.com>
2022-06-11 13:13:48 -0600

    [upgrade] Upgrade winesapOS first
    This addresses several issues with regards to repositories and
    keyrings first. This will make for a smoother upgrade process.

--- scripts/winesapos-upgrade-remote-stable.sh
+++ scripts/winesapos-upgrade-remote-stable.sh
@@ -2,16 +2,16 @@
 
 set -x
 
-kdialog --title "winesapOS Upgrade" --yesno "Do you want to first upgrade system packages?\nThis may take a long time."
+kdialog --title "winesapOS Upgrade" --yesno "Do you want to upgrade winesapOS?\nThis may take a long time."
+if [ $? -eq 0 ]; then
+    curl https://raw.githubusercontent.com/LukeShortCloud/winesapOS/stable/scripts/winesapos-upgrade.sh | sudo zsh
+fi
+
+kdialog --title "winesapOS Upgrade" --yesno "Do you want to upgrade all system packages?\nThis may take a long time."
 if [ $? -eq 0 ]; then
     sudo pacman -S -y -y -u --noconfirm
     sudo flatpak update -y --noninteractive
     yay -S -y -y -u --noconfirm
-fi
-
-kdialog --title "winesapOS Upgrade" --yesno "Do you want to upgrade winesapOS?\nThis may take a long time."
-if [ $? -eq 0 ]; then
-    curl https://raw.githubusercontent.com/LukeShortCloud/winesapOS/stable/scripts/winesapos-upgrade.sh | sudo zsh
 fi
 
 kdialog --title "winesapOS Upgrade" --msgbox "Upgrade complete."
