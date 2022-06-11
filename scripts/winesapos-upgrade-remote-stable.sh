b80e5e026cc828b0f28a1d93cacbe0619829426e
Luke Short <ekultails@gmail.com>
2022-06-11 13:11:53 -0600

    [upgrade] winesapOS upgrades may take a long time
    Let the user know before starting it.

--- scripts/winesapos-upgrade-remote-stable.sh
+++ scripts/winesapos-upgrade-remote-stable.sh
@@ -9,7 +9,7 @@ if [ $? -eq 0 ]; then
     yay -S -y -y -u --noconfirm
 fi
 
-kdialog --title "winesapOS Upgrade" --yesno "Do you want to upgrade winesapOS?"
+kdialog --title "winesapOS Upgrade" --yesno "Do you want to upgrade winesapOS?\nThis may take a long time."
 if [ $? -eq 0 ]; then
     curl https://raw.githubusercontent.com/LukeShortCloud/winesapOS/stable/scripts/winesapos-upgrade.sh | sudo zsh
 fi
