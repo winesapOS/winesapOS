a20f0de800bde4d5cb40be76b662d3fc27ba5c62
Luke Short <ekultails@gmail.com>
2022-06-12 16:07:51 -0600

    [install] Install all mesa-steamos packages
    The upstream Arch Linux packages were being installed as
    dependencies instead of mesa-steamos dependencies.
    
    Resolves #386

--- scripts/winesapos-install.sh
+++ scripts/winesapos-install.sh
@@ -269,7 +269,24 @@ echo 'MAKEFLAGS="-j $(nproc)"' >> ${WINESAPOS_INSTALL_DIR}/etc/makepkg.conf
 
 # Install 'mesa-steamos' and 'lib32-mesa-steamos' graphics driver before 'flatpak'.
 # This avoid the 'flatpak' package from installing the conflicting upstream 'mesa' package.
-arch-chroot ${WINESAPOS_INSTALL_DIR} ${CMD_PACMAN_INSTALL} winesapos/mesa-steamos winesapos/lib32-mesa-steamos
+arch-chroot ${WINESAPOS_INSTALL_DIR} ${CMD_PACMAN_INSTALL} \
+  winesapos/mesa-steamos \
+  winesapos/libva-mesa-driver-steamos \
+  winesapos/mesa-vdpau-steamos \
+  winesapos/opencl-mesa-steamos \
+  winesapos/vulkan-intel-steamos \
+  winesapos/vulkan-mesa-layers-steamos \
+  winesapos/vulkan-radeon-steamos \
+  winesapos/vulkan-swrast-steamos \
+  winesapos/lib32-mesa-steamos \
+  winesapos/lib32-libva-mesa-driver-steamos \
+  winesapos/lib32-mesa-vdpau-steamos \
+  winesapos/lib32-opencl-mesa-steamos \
+  winesapos/lib32-vulkan-intel-steamos \
+  winesapos/lib32-vulkan-mesa-layers-steamos \
+  winesapos/lib32-vulkan-radeon-steamos \
+  winesapos/lib32-vulkan-swrast-steamos
+
 # Flatpak.
 arch-chroot ${WINESAPOS_INSTALL_DIR} ${CMD_PACMAN_INSTALL} flatpak
 echo "Installing additional package managers complete."
