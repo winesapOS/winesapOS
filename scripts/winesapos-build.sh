60fa877f48d6c0a1a4900c6905e8964830673e17
Luke Short <ekultails@gmail.com>
2022-07-06 17:20:38 -0600

    [github_actions] Provide the full path
    to the build script. Otherwise, it will not be found.

--- scripts/winesapos-build.sh
+++ scripts/winesapos-build.sh
@@ -7,7 +7,7 @@ export \
     WINESAPOS_CREATE_DEVICE=true \
     WINESAPOS_BUILD_IN_VM_ONLY=false
 
-/bin/zsh scripts/winesapos-install.sh
+/bin/zsh /workdir/scripts/winesapos-install.sh
 
 ## we need to introduce versioning here
 zip -s 1900m winesapos.img.zip winesapos.img
