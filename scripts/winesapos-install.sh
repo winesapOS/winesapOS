1d9982e43a13e0a3ca8ec3ddce4d76592f928a1a
Luke Short <ekultails@gmail.com>
2023-01-24 09:58:10 -0700

    [install] Force install 'applespi'
    Resolves #551

--- scripts/winesapos-install.sh
+++ scripts/winesapos-install.sh
@@ -851,6 +851,10 @@ chroot ${WINESAPOS_INSTALL_DIR} sh -c 'git clone --branch linux5.14 https://gith
 echo "snd-hda-codec-cirrus" >> ${WINESAPOS_INSTALL_DIR}/etc/modules-load.d/winesapos-sound.conf
 # MacBook Pro Touch Bar driver.
 yay_install_chroot macbook12-spi-driver-dkms
+## Force install the driver.
+## https://github.com/LukeShortCloud/winesapOS/issues/551
+kernel_ver_lts_latest="$(ls -1 ${WINESAPOS_INSTALL_DIR}/usr/lib/modules/ | grep 5.15 | tail -n 1)"
+chroot ${WINESAPOS_INSTALL_DIR} dkms install --no-depmod macbook12-spi-driver/0+git.304 -k ${kernel_ver_lts_latest} --force
 sed -i s'/MODULES=(/MODULES=(applespi spi_pxa2xx_platform intel_lpss_pci apple_ibridge apple_ib_tb apple_ib_als /'g ${WINESAPOS_INSTALL_DIR}/etc/mkinitcpio.conf
 # iOS device management via 'usbmuxd' and a workaround required for the Touch Bar to continue to work.
 # 'uxbmuxd' and MacBook Pro Touch Bar bug reports:
