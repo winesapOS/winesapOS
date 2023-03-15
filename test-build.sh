510bd365c119027bf67b7272523c5220e457b59c
Luke Short <ekultails@gmail.com>
2023-03-15 12:03:39 -0600

    Use 'docker --pull'
    This forces 'docker' to actually download the latest Arch Linux
    image. Since it is a rolling release, using an old image will lead
    to various issues including outdated package keyrings.

--- test-build.sh
+++ test-build.sh
@@ -4,5 +4,5 @@
 # you could use the podman binary instead for testing
 
 mkdir -p output
-docker build -t winesapos-img-builder build/.
+docker build --pull --no-cache -t winesapos-img-builder build/.
 docker run --rm -v $(pwd):/workdir --privileged=true -i winesapos-img-builder:latest /bin/zsh -x scripts/winesapos-build.sh
