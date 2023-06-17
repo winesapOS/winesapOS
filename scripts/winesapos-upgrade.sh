c236c6084543928bec6b5d70bf75b6ae0cbc6a52
Luke Short <ekultails@gmail.com>
2023-06-17 17:33:15 -0600

    [upgrade] Upgrade 'filesystem'
    properly on Mac Linux Gaming Stick. Use the Manjaro package intead
    of the SteamOS one.
    
    Related #229

--- scripts/winesapos-upgrade.sh
+++ scripts/winesapos-upgrade.sh
@@ -552,7 +552,14 @@ echo "Upgrading ignored packages..."
 if [[ "${WINESAPOS_DISTRO_DETECTED}" == "arch" ]]; then
     yes | pacman -S core/linux-lts core/linux-lts-headers kernel-lts/linux-lts510 kernel-lts/linux-lts510-headers core/grub holo-rel/filesystem
 elif [[ "${WINESAPOS_DISTRO_DETECTED}" == "manjaro" ]]; then
-    yes | pacman -S core/linux515 core/linux515-headers core/linux510 core/linux510-headers core/grub holo-rel/filesystem
+    yes | pacman -S core/linux515 core/linux515-headers core/linux510 core/linux510-headers core/grub
+    # Due to conflicts between Mac Linux Gaming Stick 2 versus winesapOS 3, do not replace the 'filesystem' package.
+    # https://github.com/LukeShortCloud/winesapOS/issues/229#issuecomment-1595886615
+    if [[ "${WINESAPOS_USER_NAME}" == "stick" ]]; then
+	yes | pacman -S core/filesystem
+    else
+	yes | pacman -S holo-rel/filesystem
+    fi
 elif [[ "${WINESAPOS_DISTRO_DETECTED}" == "steamos" ]]; then
     yes | pacman -S core/linux-lts core/linux-lts-headers linux-steamos linux-steamos-headers core/grub holo-rel/filesystem
 fi
