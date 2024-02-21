4d6b8d4235d72417064d5b8200e0777eaad257f1
Luke Short <ekultails@gmail.com>
2024-02-20 21:08:17 -0700

    [test-build] Use Bash
    instead of Zsh.

--- test-build.sh
+++ test-build.sh
@@ -1,8 +1,8 @@
-#!/bin/zsh
+#!/bin/bash
 
 # this test will produce an image build from docker
 # you could use the podman binary instead for testing
 
 mkdir -p output
 docker build --pull --no-cache -t winesapos-img-builder build/.
-docker run --rm -v $(pwd):/workdir --privileged=true -i winesapos-img-builder:latest /bin/zsh -x scripts/winesapos-build.sh
+docker run --rm -v $(pwd):/workdir --privileged=true -i winesapos-img-builder:latest /bin/bash -x scripts/winesapos-build.sh
