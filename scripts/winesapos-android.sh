e23d9b5f250431f609e3e15a5c8f500d9795a943
Xaryx <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-07-11 17:29:48 +0400

    Create winesapos-android.sh

---
+++ scripts/winesapos-android.sh
@@ -0,0 +1,13 @@
+echo "This may take a while to install dependencies"
+# This part will install yay
+cd ~/
+sudo pacman -S git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+echo "Time To install Android"
+# DONT REMOVE THESE LINES PLEASE THIS WILL BREAK EVERyTHING
+yay -S waydroid
+yay -S waydroid-image-gapps
+yay -S waydroid-image
+echo "Complete you may close this safely"
