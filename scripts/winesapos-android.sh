ff8fb12ea797d655f96ad2faa013178aaa0bd46e
Xaryx <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-07-20 21:31:14 +0400

    Update winesapos-android.sh

--- scripts/winesapos-android.sh
+++ scripts/winesapos-android.sh
@@ -1,8 +1,9 @@
 echo "This may take a while to install"
 echo "installing Android"
 # DONT REMOVE THESE LINES PLEASE THIS WILL BREAK EVERYTHING
-yay -S waydroid
-yay -S waydroid-image-gapps
-yay -S waydroid-image
+makepkg_fn waydroid install
+makepkg_fn waydroid-image-gapps install
+makepkg_fn waydroid-image
+echo "Starting Container"
 sudo systemctl enable --now waydroid-container
 echo "Complete you may close this safely"
