ad658610e13a00005d37eae396b94862abb36fb7
Xaryx <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-08-08 20:59:57 +0400

    Update winesapos-build-repo.sh

--- scripts/repo/winesapos-build-repo.sh
+++ scripts/repo/winesapos-build-repo.sh
@@ -78,7 +78,9 @@ makepkg_fn inputplumber-bin install
 makepkg_fn powerstation-bin install
 makepkg_fn opengamepadui-bin install
 makepkg_fn opengamepadui-session-git
+# Virtual Machine Containers
 makepkg_fn podman-desktop
+makepkg_fn docker-desktop
 
 # 'inputmodule-udev' is a dependency for 'inputmodule-control'.
 makepkg_fn inputmodule-udev install
