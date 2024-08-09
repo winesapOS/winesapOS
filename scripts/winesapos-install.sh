129bcc2441ba61efde2507ab67a259ca23288aa8
Xaryx <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-08-09 21:17:29 +0400

    Update winesapos-install.sh

--- scripts/winesapos-install.sh
+++ scripts/winesapos-install.sh
@@ -363,9 +363,8 @@ pacman_install_chroot inetutils
 # Install fingerprint scanning support.
 pacman_install_chroot fprintd
 echo "Installing ${WINESAPOS_DISTRO} complete."
-# GNOME Boxes support
+# GNOME Boxes and Virtual Machine Manager support.
 pacman_install_chroot spice-vdagent
-
 echo "Setting up Pacman parallel package downloads in chroot..."
 # Increase from the default 1 package download at a time to 5.
 sed -i s'/\#ParallelDownloads.*/ParallelDownloads=5/'g ${WINESAPOS_INSTALL_DIR}/etc/pacman.conf
