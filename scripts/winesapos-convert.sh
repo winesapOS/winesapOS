b6567177380637e20c2e4bc6dcf44da398ace2f5
Luke Short <ekultails@gmail.com>
2024-08-18 09:09:01 -0600

    [convert] Better manage repositories
    Only add a repository if it is not configured yet to prevent
    duplicate entries if the conversion script is re-ran.
    
    Also add the Chaotic AUR repository.

--- scripts/winesapos-convert.sh
+++ scripts/winesapos-convert.sh
@@ -7,21 +7,44 @@ sleep 1
 echo "Converting system to winesapOS in 1"
 sleep 1
 echo "System is converting ..."
-pacman -Sy --noconfirm
+pacman -S -y
 useradd -m winesap
 echo "winesap:winesap" | chpasswd
 pacman -S git wget flatpak base-devel --noconfirm
 curl https://raw.githubusercontent.com/LukeShortCloud/winesapOS/stable/files/os-release-winesapos --location --output /usr/lib/os-release-winesapos
 ln -s /usr/lib/os-release-winesapos /etc/os-release-winesapos
 
-echo "[winesapos]" >> /etc/pacman.conf
-echo "Server = https://winesapos.lukeshort.cloud/repo/winesapos/\$arch/" >> /etc/pacman.conf
-echo "Importing the public GPG key for the winesapOS repository..."
-chroot ${WINESAPOS_INSTALL_DIR} pacman-key --recv-keys 1805E886BECCCEA99EDF55F081CA29E4A4B01239
-chroot ${WINESAPOS_INSTALL_DIR} pacman-key --init
-chroot ${WINESAPOS_INSTALL_DIR} pacman-key --lsign-key 1805E886BECCCEA99EDF55F081CA29E4A4B01239
-echo "Importing the public GPG key for the winesapOS repository complete."
-echo "Adding the winesapOS repository complete."
+grep "[winesapos]" /etc/pacman.conf
+if [ $? -ne 0 ]; then
+    echo "Adding the winesapOS repository..."
+    echo "[winesapos]" >> /etc/pacman.conf
+    echo "Server = https://winesapos.lukeshort.cloud/repo/winesapos/\$arch/" >> /etc/pacman.conf
+    echo "Importing the public GPG key for the winesapOS repository..."
+    pacman-key --recv-keys 1805E886BECCCEA99EDF55F081CA29E4A4B01239
+    pacman-key --init
+    pacman-key --lsign-key 1805E886BECCCEA99EDF55F081CA29E4A4B01239
+    echo "Importing the public GPG key for the winesapOS repository complete."
+    pacman -S -y
+    echo "Adding the winesapOS repository complete."
+fi
+
+grep "[chaotic-aur]" /etc/pacman.conf
+if [ $? -ne 0 ]; then
+    # https://aur.chaotic.cx/
+    echo "Adding the Chaotic AUR repository..."
+    pacman-key --recv-keys 3056513887B78AEB --keyserver keyserver.ubuntu.com
+    pacman-key --lsign-key 3056513887B78AEB
+    wget 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' -LO /tmp/chaotic-keyring.pkg.tar.zst
+    pacman --noconfirm -U /tmp/chaotic-keyring.pkg.tar.zst
+    wget 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' -LO /tmp/chaotic-mirrorlist.pkg.tar.zst
+    pacman --noconfirm -U /chaotic-mirrorlist.pkg.tar.zst
+    rm -f /tmp/chaotic-*.pkg.tar.zst
+    echo "
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+    pacman -S -y
+    echo "Adding the Chaotic AUR repository complete."
+fi
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
