a9a6f6de7d92b7a1be029c3ecf3330e508ce232c
Luke Short <ekultails@gmail.com>
2024-08-18 00:35:32 -0600

    [convert] Use newlines
    for each package to be installed. This makes it easier to read and
    modify.

--- scripts/winesapos-convert.sh
+++ scripts/winesapos-convert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Script by @GuestSneezeOSDev
-echo "Convering system to winesapOS in 3"
+# Script originally created by @GuestSneezeOSDev
+echo "Converting system to winesapOS in 3"
 sleep 1
-echo "Convering system to winesapOS in 2"
+echo "Converting system to winesapOS in 2"
 sleep 1
-echo "Convering system to winesapOS in 1"
+echo "Converting system to winesapOS in 1"
 sleep 1
 echo "System is converting ..."
 pacman -Sy --noconfirm
@@ -22,12 +22,52 @@ chroot ${WINESAPOS_INSTALL_DIR} pacman-key --init
 chroot ${WINESAPOS_INSTALL_DIR} pacman-key --lsign-key 1805E886BECCCEA99EDF55F081CA29E4A4B01239
 echo "Importing the public GPG key for the winesapOS repository complete."
 echo "Adding the winesapOS repository complete."
-grep pacman_install scripts/winesapos-install.sh
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd ..
 rm -rf yay
-yay -S --noconfirm waydroid waydroid-biglinux waydroid-image waydroid-image-gapps waydroid-image-halium waydroid-magisk waydroid-openrc google-chrome protonup-qt heroic-games-launcher heroic-games-launcher-bin prismlauncher prismlauncher-bin lutris-wine-meta
-flatpak install --noninteractive io.github.antimicrox.antimicrox com.usebottles.bottles com.calibre_ebook.calibre org.gnome.Cheese com.gitlab.davem.ClamTk com.discordapp.Discord org.filezillaproject.Filezilla com.github.tchx84.Flatseal org.freedesktop.Platform.VulkanLayer.gamescope com.google.Chrome com.heroicgameslauncher.hgl org.keepassxc.KeePassXC org.libreoffice.LibreOffice net.lutris.Lutris org.freedesktop.Platform.VulkanLayer.MangoHud com.obsproject.Studio io.github.peazip.PeaZip org.prismlauncher.PrismLauncher com.github.Matoking.protontricks net.davidotek.pupgui2 org.qbittorrent.qBittorrent com.valvesoftware.Steam com.valvesoftware.Steam.Utility.steamtinkerlaunch org.videolan.VLC
-echo "System is converting ... Conversion completed successfully"
+yay -S --noconfirm \
+  google-chrome \
+  heroic-games-launcher \
+  heroic-games-launcher-bin \
+  lutris-wine-meta \
+  prismlauncher \
+  prismlauncher-bin \
+  protonup-qt \
+  waydroid \
+  waydroid-biglinux \
+  waydroid-image \
+  waydroid-image-gapps \
+  waydroid-image-halium \
+  waydroid-magisk \
+  waydroid-openrc
+
+flatpak install --noninteractive \
+  io.github.antimicrox.antimicrox \
+  com.usebottles.bottles \
+  com.calibre_ebook.calibre \
+  org.gnome.Cheese \
+  com.gitlab.davem.ClamTk \
+  com.discordapp.Discord \
+  org.filezillaproject.Filezilla \
+  com.github.tchx84.Flatseal \
+  org.freedesktop.Platform.VulkanLayer.gamescope \
+  com.google.Chrome \
+  com.heroicgameslauncher.hgl \
+  org.keepassxc.KeePassXC \
+  org.libreoffice.LibreOffice \
+  net.lutris.Lutris \
+  org.freedesktop.Platform.VulkanLayer.MangoHud \
+  com.obsproject.Studio \
+  io.github.peazip.PeaZip \
+  org.prismlauncher.PrismLauncher \
+  com.github.Matoking.protontricks \
+  net.davidotek.pupgui2 \
+  org.qbittorrent.qBittorrent \
+  com.valvesoftware.Steam \
+  com.valvesoftware.Steam.Utility.steamtinkerlaunch \
+  org.videolan.VLC
+
+echo "Conversion to winesapOS completed successfully"
