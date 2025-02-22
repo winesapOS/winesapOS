12a17f7f1470750a11bd233ba95c8e9e43e27042
Luke Short <ekultails@gmail.com>
2025-02-21 19:04:02 -0700

    [install] Update Balena Etcher
    from 1.19.21 to 1.19.25.

--- scripts/winesapos-install.sh
+++ scripts/winesapos-install.sh
@@ -583,7 +583,7 @@ echo "Installing sound drivers complete."
 
 echo "Installing balenaEtcher..."
 # Etcher by balena.
-export ETCHER_VER="1.19.21"
+export ETCHER_VER="1.19.25"
 curl --location "https://github.com/balena-io/etcher/releases/download/v${ETCHER_VER}/balenaEtcher-${ETCHER_VER}-x64.AppImage" --output "${WINESAPOS_INSTALL_DIR}"/home/"${WINESAPOS_USER_NAME}"/Desktop/balenaEtcher.AppImage
 chmod +x "${WINESAPOS_INSTALL_DIR}"/home/"${WINESAPOS_USER_NAME}"/Desktop/balenaEtcher.AppImage
 echo "Installing balenaEtcher complete."
