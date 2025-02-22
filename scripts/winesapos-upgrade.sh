a18452d31d6ce6cf7503a124409654a22bdf291d
Luke Short <ekultails@gmail.com>
2025-02-21 19:06:30 -0700

    [upgrade] Update 'static-curl'
    from 8.11.0 to 8.12.1.

--- scripts/winesapos-upgrade.sh
+++ scripts/winesapos-upgrade.sh
@@ -29,7 +29,7 @@ install_curl_static() {
         if ! /usr/bin/pacman --noconfirm -S curl-static-bin; then
             export CMD_CURL=/usr/local/bin/curl-static
             if ! "${CMD_CURL}" --version &> /dev/null; then
-                CURL_STATIC_VER="8.11.0"
+                CURL_STATIC_VER="8.12.1"
                 curl --location --remote-name "https://github.com/stunnel/static-curl/releases/download/${CURL_STATIC_VER}/curl-linux-x86_64-musl-${CURL_STATIC_VER}.tar.xz" --output-dir /tmp/
                 tar -xvf "/tmp/curl-linux-x86_64-musl-${CURL_STATIC_VER}.tar.xz" -C /tmp/
                 mv /tmp/curl "${CMD_CURL}"
