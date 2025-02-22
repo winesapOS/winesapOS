2b5e17e5e6b8021b4c8a0895573ace2329a11ffc
Luke Short <ekultails@gmail.com>
2025-02-21 19:11:05 -0700

    [wsl] Update winesapOS version
    from 4.2.0 to 4.3.0.

--- scripts/winesapos-wsl.ps1
+++ scripts/winesapos-wsl.ps1
@@ -3,7 +3,7 @@
 # OR
 # ask the user which version (4.1.0 - Latest version)
 $installPath = "$env:LOCALAPPDATA\Packages\winesapos"
-$winesaposTarballCompressedUrl = "https://winesapos.lukeshort.cloud/repo/iso/winesapos-4.2.0/winesapos-4.2.0-minimal-rootfs.tar.zst"
+$winesaposTarballCompressedUrl = "https://winesapos.lukeshort.cloud/repo/iso/winesapos-4.3.0/winesapos-4.3.0-minimal-rootfs.tar.zst"
 $winesaposTarballCompressedPath = "$installPath\winesapos.tar.zst"
 $winesaposTarballUncompressedPath = "$installPath\winesapos.tar"
 $zstdUrl = "https://github.com/facebook/zstd/releases/download/v1.5.5/zstd-v1.5.5-win64.zip"
