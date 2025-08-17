77b65af4d5408d08e25655a2846d29d521bcacdd
Luke Short <ekultails@gmail.com>
2025-08-17 17:16:48 -0600

    chore(test-build): remove legacy build script
    Instructions are provided in the CONTRIBUTING.md document on how to
    do a modern build using a container.

--- test-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# this test will produce an image build from docker
-# you could use the podman binary instead for testing
-
-mkdir -p output
-docker build --pull --no-cache -t winesapos-img-builder build/.
-docker run --rm -v "$(pwd)":/workdir --privileged=true -i winesapos-img-builder:latest /bin/bash -x scripts/winesapos-build.sh
