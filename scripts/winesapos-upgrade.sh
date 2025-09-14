737737de14e644530937276db7a5aa46f8ba87b8
Luke Short <ekultails@gmail.com>
2025-09-14 09:53:05 -0600

    fix(upgrade): disable cancel button
    during winesapOS 4.3.0 to 4.4.0 upgrades.

--- scripts/winesapos-upgrade.sh
+++ scripts/winesapos-upgrade.sh
@@ -1129,6 +1129,7 @@ echo "Running 4.2.0 to 4.3.0 upgrades complete."
 
 echo "Running 4.3.0 to 4.4.0 upgrades..."
 kdialog_dbus=$(sudo -E -u "${WINESAPOS_USER_NAME}" kdialog --title "winesapOS Upgrade" --progressbar "Running 4.3.0 to 4.4.0 upgrades..." 3 | cut -d" " -f1)
+sudo -E -u "${WINESAPOS_USER_NAME}" "${qdbus_cmd}" "${kdialog_dbus}" /ProgressDialog showCancelButton false
 
 if ${CMD_PACMAN} -Q asusctl-git; then
     "${CMD_PACMAN_REMOVE[@]}" asusctl-git
