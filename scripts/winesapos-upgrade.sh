fe4b2d7e1d9d22720b5d9fedb37d40d591ba1293
Luke Short <ekultails@gmail.com>
2026-08-16 09:44:09 -0600

    fix(upgrade): handle new 'goverlay-git' version upgrades
    by removing the old 'pascube' dependency.

--- scripts/winesapos-upgrade.sh
+++ scripts/winesapos-upgrade.sh
@@ -1049,6 +1049,10 @@ sudo -E -u "${WINESAPOS_USER_NAME}" "${qdbus_cmd}" "${kdialog_dbus}" /ProgressDi
 
 if ${CMD_PACMAN} -Q mangohud; then
     "${CMD_PACMAN_REMOVE[@]}" mangohud lib32-mangohud goverlay
+    # 'goverlay-git' 1.8.10 and newer now conflict and replace the previous 'pascube' dependency because it is bundled now.
+    if ${CMD_PACMAN} -Q pascube; then
+        ${CMD_PACMAN} -R -n --nodeps --nodeps --noconfirm pascube
+    fi
     "${CMD_PACMAN_INSTALL[@]}" mangohud-git lib32-mangohud-git goverlay-git
 fi
 sudo -E -u "${WINESAPOS_USER_NAME}" "${qdbus_cmd}" "${kdialog_dbus}" /ProgressDialog Set org.kde.kdialog.ProgressDialog value 2
@@ -1303,7 +1307,7 @@ sudo -E -u "${WINESAPOS_USER_NAME}" "${qdbus_cmd}" "${kdialog_dbus}" /ProgressDi
 echo "Running 4.4.0 to 4.5.0 upgrades complete."
 
 
-kdialog_dbus=$(sudo -E -u "${WINESAPOS_USER_NAME}" kdialog --title "winesapOS Upgrade" --progressbar "Running 4.5.0 to 4.6.0 upgrades..." 9 | cut -d" " -f1)
+kdialog_dbus=$(sudo -E -u "${WINESAPOS_USER_NAME}" kdialog --title "winesapOS Upgrade" --progressbar "Running 4.5.0 to 4.6.0 upgrades..." 10 | cut -d" " -f1)
 sudo -E -u "${WINESAPOS_USER_NAME}" "${qdbus_cmd}" "${kdialog_dbus}" /ProgressDialog showCancelButton false
 
 if ${CMD_PACMAN} -Q kdsoap-qt5; then
@@ -1416,6 +1420,12 @@ sudo -E -u "${WINESAPOS_USER_NAME}" "${qdbus_cmd}" "${kdialog_dbus}" /ProgressDi
 
 if ${CMD_PACMAN} -Q appimagelauncher; then
     "${CMD_PACMAN_REMOVE[@]}" appimagelauncher
+fi
+sudo -E -u "${WINESAPOS_USER_NAME}" "${qdbus_cmd}" "${kdialog_dbus}" /ProgressDialog Set org.kde.kdialog.ProgressDialog value 9
+
+# 'goverlay-git' 1.8.10 and newer now conflict and replace the previous 'pascube' dependency because it is bundled now.
+if ${CMD_PACMAN} -Q pascube; then
+    ${CMD_PACMAN} -R -n --nodeps --nodeps --noconfirm pascube
 fi
 sudo -E -u "${WINESAPOS_USER_NAME}" "${qdbus_cmd}" "${kdialog_dbus}" /ProgressDialog org.kde.kdialog.ProgressDialog.close
 echo "Running 4.5.0 to 4.6.0 upgrades complete."
